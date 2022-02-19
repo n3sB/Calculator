626483209c73e02002d3422f374882f13b40dd61
N3sB <enesbaysl@outlook.com>
2022-02-19 14:02:46 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,6 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
         int number1, number2, select;
 
         System.out.print("Please enter the first number: ");
@@ -13,9 +12,7 @@ public class Calculator {
         System.out.print("Please enter the second number: ");
         number2 = scanner.nextInt();
 
-
         System.out.println("*****       ******");
-
 
         System.out.println("1-Addition\n2-Extraction\n3-Division\n4-Multiplication");
 
@@ -23,26 +20,26 @@ public class Calculator {
         select = scanner.nextInt();
         scanner.close();
 
-        switch(select){
+        switch (select) {
 
-            case 1 :
-                    System.out.println("Addition: " + (number1+number2));
+            case 1:
+                System.out.println("Addition: " + (number1 + number2));
                 break;
-            case 2 :
-                    System.out.println("Extraction: " + (number1-number2));
+            case 2:
+                System.out.println("Extraction: " + (number1 - number2));
                 break;
-            case 3 :
-                    System.out.println("Division: " + (number1/number2));
+            case 3:
+                System.out.println("Division: " + (number1 / number2));
                 break;
-            case 4 :
-                if(number2 != 0){
+            case 4:
+                if (number2 != 0) {
                     System.out.println("A number cannot be divided by 0.");
-                }else{
-                    System.out.println("Multiplication: " + (number1*number2) );
+                } else {
+                    System.out.println("Multiplication: " + (number1 * number2));
                 }
                 break;
-            default :
-                    System.out.println("Invalic choice. Please try again.");
+            default:
+                System.out.println("Invalic choice. Please try again.");
         }
 
     }
